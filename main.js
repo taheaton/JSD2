f5f373b7ff6adaab3a93e9e60c0df028bc9847e8
Thomas Heaton <thomasaheaton@gmail.com>
2015-09-30 08:35:38 -0400

    changes made

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//Add function
+
 function Calc1(){
 VField1=eval(document.calcform.field1.value)
 VField2=eval(document.calcform.field2.value)
@@ -5,6 +7,7 @@ d=VField1+VField2
 document.calcform.field3output.value = d;
 }
 
+//Subtract function
 function Calc2(){
 VField1=eval(document.calcform.field1.value)
 VField2=eval(document.calcform.field2.value)
@@ -12,13 +15,14 @@ d=VField1-VField2
 document.calcform.field3output.value = d;
 }
 
+//Multiply function
 function Calc3(){
 VField1=eval(document.calcform.field1.value)
 VField2=eval(document.calcform.field2.value)
 d=VField1*VField2
 document.calcform.field3output.value = d;
 }
-
+//Divide function
 function Calc4(){
 VField1=eval(document.calcform.field1.value)
 VField2=eval(document.calcform.field2.value)
